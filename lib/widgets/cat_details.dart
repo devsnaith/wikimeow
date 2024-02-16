9bc108764d426d30be111dba661a4b8875404fe8
강남스타일 <65865554+devsnaith@users.noreply.github.com>
2024-02-16 04:02:15 +0300

    Update cat_details.dart

--- lib/widgets/cat_details.dart
+++ lib/widgets/cat_details.dart
@@ -3,24 +3,35 @@ import 'package:wikiMeow/widgets/panels/CatDetailsWidget.dart';
 import 'package:wikiMeow/widgets/secret_widget.dart';
 import 'package:flutter/material.dart';
 
-class CatDetails extends StatelessWidget {
+class CatDetails extends StatefulWidget {
   const CatDetails({super.key, required this.catModel});
   final CatModel catModel;
 
+  @override
+  State<CatDetails> createState() => _CatDetailsState();
+}
+
+class _CatDetailsState extends State<CatDetails> {
+  bool floatinBackgroundColorBlack = true;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.indigo[50],
         floatingActionButton: FloatingActionButton.extended(
-          backgroundColor: Colors.black,
-          label: TextButton.icon(onPressed: () => {},
+          backgroundColor: floatinBackgroundColorBlack ? Colors.black : Colors.pink,
+          label: TextButton.icon(onPressed: () => {
+            setState(() {
+              floatinBackgroundColorBlack = !floatinBackgroundColorBlack;
+            })
+          },
           label: const Text("Add to Favorite", style: TextStyle(color: Colors.white),),
           icon: const Icon(Icons.favorite, color: Colors.white,)),
-          onPressed: () => {}, 
+          onPressed: () => {
+          }, 
         ),
         appBar: AppBar(
-          title: Center(child: Text(catModel.name!, style: const TextStyle(fontWeight: FontWeight.bold))),
+          title: Center(child: Text(widget.catModel.name!, style: const TextStyle(fontWeight: FontWeight.bold))),
           leading: IconButton(onPressed: () => {
             Navigator.of(context).pop()
           }, icon: const Icon(Icons.navigate_before)),
@@ -35,7 +46,7 @@ class CatDetails extends StatelessWidget {
             )
           ],
         ),
-        body: CatDetailsWidget(catModel: catModel),
+        body: CatDetailsWidget(catModel: widget.catModel),
         ),
     );
   }
